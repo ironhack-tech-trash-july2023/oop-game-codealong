95081c332d1f376025d576824ede6280bba05363
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2023-08-10 16:33:44 +0200

    create and move obstacles

--- js/main.js
+++ js/main.js
@@ -34,7 +34,60 @@ class Player {
 }
 
 
+class Obstacle {
+    constructor(){
+        this.positionX = 50;
+        this.positionY = 100;
+        this.width = 20;
+        this.height = 10;
+        this.domElement = null;
+
+        this.createDomElement();
+    }
+    createDomElement(){
+        // create dom element
+        this.domElement = document.createElement("div");
+
+        // set id
+        this.domElement.className = "obstacle";
+        this.domElement.style.width = this.width + "vw";
+        this.domElement.style.height = this.height + "vh";
+        this.domElement.style.left = this.positionX + "vw";
+        this.domElement.style.bottom = this.positionY + "vh";
+
+        //append to the dom
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.domElement);
+    }
+    moveDown(){
+        this.positionY -= 2;
+        this.domElement.style.bottom = this.positionY + "vh";
+    }
+}
+
+
+
 const player = new Player();
+
+
+const obstaclesArr = [];  //will store instances of the class Obstacle
+
+
+
+// create obstacles
+setInterval(() => {
+    const newObstacle = new Obstacle();
+    obstaclesArr.push(newObstacle);
+}, 3000);
+
+
+// move all obstacles
+setInterval(() => {
+    obstaclesArr.forEach( (obstacleInstance) => {
+        obstacleInstance.moveDown();
+    });
+}, 100);
+
 
 
 document.addEventListener("keydown", (event) => {
